49b543da494ec8393d200ec444f8cb158652fb7b
ribbon-otter <ribbon-otter@github.invalid>
2026-06-18 20:42:09 -0700

    basic implementation

---
+++ src/counter.rs
@@ -0,0 +1,273 @@
+// based on https://docs.rs/crate/counter
+use rustc_hash::FxBuildHasher;
+use rustc_hash::FxHashMap;
+use std::ops::{MulAssign, AddAssign, Index, *};
+use std::cmp::Eq;
+use std::hash::Hash;
+use std::iter::Sum;
+use std::fmt::Debug;
+
+use rayon::prelude::*; 
+
+
+
+#[derive(Clone, Debug)]
+pub struct Counter<T> {
+	map: FxHashMap<T, usize>,
+	//cache of the current most common value 
+	//
+	//it's public so you can manually fix it
+	//if you break it with mutable indexing
+	pub current_max : Option<(T, usize)>,
+}
+
+impl<T> IntoParallelIterator for Counter<T> 
+where T: Hash + Eq + Clone + Send + Sync {
+
+	type Iter = <FxHashMap<T, usize> as IntoParallelIterator>::Iter;
+	type Item = <FxHashMap<T, usize> as IntoParallelIterator>::Item;
+
+	fn into_par_iter(self) -> Self::Iter {
+		self.map.into_par_iter()
+	}
+}
+
+impl<'data, T> IntoParallelRefIterator<'data> for Counter<T>
+where T: 'data + Hash + Eq + Clone + Sync {
+
+	type Iter = <&'data FxHashMap<T, usize> as IntoParallelIterator>::Iter;
+	type Item = <&'data FxHashMap<T, usize> as IntoParallelIterator>::Item;
+
+	fn par_iter(&'data self) -> Self::Iter {
+		self.map.par_iter()
+	}
+}
+
+
+impl<'a, T> IntoIterator for &'a Counter<T>
+where T: 'a + Hash + Eq + Clone {
+		type Item = (&'a T, &'a usize);
+		type IntoIter = std::collections::hash_map::Iter<'a, T, usize>;
+
+		// Required method
+		fn into_iter(self) -> Self::IntoIter {
+			self.map.iter()
+		}
+}
+
+impl<T: std::cmp::Eq> PartialEq for Counter<T>
+where T: Hash + Eq
+{
+	fn eq(&self, other: &Self) -> bool {
+		// ingore the zero
+		self.map == other.map
+	}
+}
+
+impl<T> Counter<T> 
+where T: Hash + Eq + Clone + Sync
+{
+	pub fn new() -> Self {
+		Counter {
+			map: FxHashMap::<T, usize>::default(),
+			current_max : None
+		}
+	}
+	
+	pub fn with_capacity(capacity: usize) -> Self {
+		use std::collections::HashMap;
+		Counter {
+			map: HashMap::with_capacity_and_hasher(capacity, FxBuildHasher::default()),
+			current_max: None
+		}
+	}
+
+	pub fn update<I>(&mut self, iterable: I) 
+	where
+		I: IntoIterator<Item = T>,
+	{
+		for item in iterable {
+			let entry = self.map.entry(item.clone()).or_insert(0);
+			*entry += 1;
+			let value = *entry;
+			private_check_current_max(&mut self.current_max, &item, value);
+			}
+		}
+
+	pub fn most_common(&self) -> Option<(T, usize)>
+	{
+		self.current_max.clone()
+	}
+
+	pub fn len(&self) -> usize {
+		self.map.len()
+	}
+
+	pub fn is_empty(&self) -> bool {
+		self.map.is_empty()
+	}
+	///total number of items which have been counted
+	pub fn total(&self) -> usize {
+		self.par_iter().map(|(_key, value)| value).sum()
+	}
+}
+
+//used to restore current_max invariant in some functions
+fn private_check_current_max<T>(current_max : &mut Option<(T, usize)>, key : &T, value: usize) where T: Hash + Eq + Clone {
+	if let Some((_key, count)) = current_max {
+		if value > *count {
+			*current_max = Some((key.clone(), value));
+		}
+	} else {
+			*current_max = Some((key.clone(), value));
+	}
+}
+
+impl<T> Sum for Counter<T> 
+where T: Hash + Eq + Clone + Debug + Sync {
+	fn sum<I>(iter: I) -> Self
+		where I: Iterator<Item = Counter<T>> {
+		let mut result = Counter::new(); 
+		for i in iter {
+			result += i;
+		}
+		result
+	}
+}
+
+impl<T> AddAssign<Counter<T>> for Counter<T> 
+where T: Hash + Eq + Clone + Debug {
+	fn add_assign(&mut self, rhs: Counter<T>) {
+		for (key, val) in rhs {
+			self[&key] += val;
+			let new_total = self[&key];
+			private_check_current_max(&mut self.current_max, &key, new_total);
+		}
+	}
+}
+
+static ZERO : usize = 0;
+
+impl<T> Index<&T> for Counter<T>
+where T: Hash + Eq {
+	type Output = usize;
+	fn index(&self, index: &T) -> &Self::Output {
+		&self.map.get(index).unwrap_or(&ZERO)
+	}
+}
+
+///watch out! you need to manually fix 
+impl<T> IndexMut<&T> for Counter<T>
+where T: Hash + Eq + Clone {
+	fn index_mut(&mut self, index: &T) -> &mut Self::Output {
+		self.map.entry((*index).clone()).or_insert(0)
+	}
+}
+
+impl<T> MulAssign<usize> for Counter<T> {
+	fn mul_assign(&mut self, rhs: usize) {
+		for (_, val) in &mut self.map {
+			*val *= rhs; 
+		}
+		if let Some(cm) =  &mut self.current_max {
+			//multiplying preserves maximiums so we only need to update
+			cm.1 *= 2;
+		}
+	}
+}
+
+impl<T> Mul<usize> for Counter<T> {
+	type Output = Counter<T>;
+	fn mul(mut self, rhs: usize) -> Counter<T> {
+		for (_, val) in &mut self.map {
+			*val *= rhs
+		}
+		if let Some(max) = &mut self.current_max {
+			(*max).1 *= rhs;
+		}
+		self
+	}
+}
+
+impl<T> IntoIterator for Counter<T> {
+	type Item = (T, usize);
+	type IntoIter = std::collections::hash_map::IntoIter<T, usize>;
+	fn into_iter(self) -> Self::IntoIter {
+		self.map.into_iter()
+	}
+}
+
+impl<'a, T, > IntoIterator for &'a mut Counter<T> {
+	type Item = (&'a T, &'a mut usize);
+	type IntoIter = std::collections::hash_map::IterMut<'a, T, usize>;
+	fn into_iter(self) -> Self::IntoIter {
+		self.map.iter_mut()
+	}
+}
+
+impl<T> FromIterator<T> for Counter<T>
+where T: Hash + Eq + Clone + Sync,
+{
+	fn from_iter<I: IntoIterator<Item = T>>(iterable: I) -> Self {
+		let mut counter = Counter::new();
+		counter.update(iterable);
+		counter
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	
+	#[test]
+	fn test_basic() {
+		let mut c = Counter::new();
+		c.update(vec!("a", "b", "a"));
+		assert_eq!(c.most_common().unwrap().1, 2);
+		assert_eq!(c.most_common().unwrap().0, "a");
+		c.update(vec!("b", "b", "b"));
+		assert_eq!(c.most_common().unwrap().1, 4);
+		assert_eq!(c.most_common().unwrap().0, "b");
+	}
+	#[test]
+	fn test_index() {
+		let mut c = Counter::new();
+		c.update(vec!("a", "b", "a"));
+		assert_eq!(c[&"a"], 2);
+		assert_eq!(c[&"c"], 0);
+		assert_eq!(c[&"b"], 1);
+	}
+	#[test]
+	fn test_add() {
+		let mut c = Counter::new();
+		c.update(vec!("a", "b", "a"));
+		let mut d = Counter::new();
+		d.update(vec!("a", "b", "a", "a", "c"));
+		c += d;
+		assert_eq!(c[&"a"], 5);
+		assert_eq!(c[&"b"], 2);
+		assert_eq!(c[&"c"], 1);
+		assert_eq!(c.most_common().unwrap().1, 5);
+		assert_eq!(c.most_common().unwrap().0, "a");
+	}
+	#[test]
+	fn test_mul() {
+		let mut c = Counter::new();
+		c.update(vec!("a", "b", "a"));
+		c *= 2;
+		assert_eq!(c.most_common().unwrap().1, 4);
+		assert_eq!(c.most_common().unwrap().0, "a");
+	}
+	#[test]
+	fn test_sum() {
+		let mut c = Counter::new();
+		c.update(vec!("a", "b", "a"));
+		let v : Vec<Counter<_>> = vec!(c.clone(), c.clone());
+		let vv : Counter<_> = v.into_iter().sum();
+		assert_eq!(vv.most_common().unwrap().1, 4);
+		assert_eq!(vv.most_common().unwrap().0, "a");
+
+	}
+}
+
+// vim: ts=2 sw=2
---
+++ src/main.rs
@@ -0,0 +1,294 @@
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use rustc_hash::FxHashMap;
+
+mod counter;
+use counter::Counter;
+
+use rayon::prelude::*; 
+
+type Token = Vec<u16>;
+
+///extention_population * EXT_RATIO > candidate_population 
+///triggers using the extention
+static EXT_RATIO : usize = 2;
+
+static NEW_TOKEN_COUNT : u16 = 1000;
+static BASE_TOKENS : u16 = 256; //must be 256 by other assumptions in code
+//token numbers from 0 to 255 (inclusive) represent the raw bytes
+//while tokens greater than that represent compressions
+
+fn main() -> io::Result<()> {
+	let library = gen_word_counts("./iliad-modern-greek.txt")?;
+	let (vocab, compressed_lib) = bpe_optimized(library.clone());
+	let opt_fertility =  fertility(&compressed_lib);
+	println!("optimized bpe : fertility {}", fertility(&compressed_lib));
+	println!("optimized bpe: {:?}", vocab.iter().take(10).map(to_string).collect::<Vec<_>>());
+	let (vocab, compressed_lib) = bpe(library.clone());
+	let old_fertility =  fertility(&compressed_lib);
+	println!("bpe : fertility {}", old_fertility);
+	println!("bpe: {:?}", vocab.iter().take(10).map(to_string).collect::<Vec<_>>());
+	println!();
+	println!("improvement ratio: {}", opt_fertility / old_fertility);
+	Ok(())
+}
+
+fn bpe(mut library : Counter<Token>) -> (Vec<Token>, Counter<Token>){
+	//vocab[i] is the expantion of token number (i - base_tokens)
+	let mut vocab : Vec<Token> = Vec::with_capacity(NEW_TOKEN_COUNT.into());
+	for i in 0..NEW_TOKEN_COUNT {
+		let new_token = find_candidate(&library).0;
+		library = replace_in_library(&library, &new_token, i + BASE_TOKENS);
+		vocab.push(new_token);
+	}
+	(vocab, library)
+}
+
+fn bpe_optimized(mut library : Counter<Token>) -> (Vec<Token>, Counter<Token>) {
+	//vocab[i] is the expantion of token number (i - base_tokens)
+	let mut vocab : Vec<Token> = Vec::with_capacity(NEW_TOKEN_COUNT.into());
+	for i in 0..NEW_TOKEN_COUNT {
+		let new_token = find_best_token(&library).0;
+		library = replace_in_library(&library, &new_token, i + BASE_TOKENS);
+		vocab.push(new_token);
+	}
+	(vocab, library)
+}
+
+//replaces a string with a single value
+fn replace(s : &[u16], from: &[u16], to : u16) -> Vec<u16> {
+	assert!(from.len() > 0);
+	let mut result : Vec<u16> = Vec::new();
+	let mut i : usize = 0;
+	while i < s.len() {
+		if i+from.len()-1 < s.len() && s[i..i+from.len()] == *from {
+			result.push(to);
+			i += from.len()
+		} else {
+			result.push(s[i]);
+			i+=1;
+		}
+	}
+	result
+}
+
+fn replace_in_library(library : &Counter<Token>, from : &[u16], to : u16) -> Counter<Token> {
+	let mut new_library : Counter<Token> = Counter::with_capacity(library.len());
+	for (key, count) in library {
+		let new_key = replace(key, from, to);
+		new_library[&new_key] = *count;
+	}
+	if let Some(cm) = &library.current_max {
+		new_library.current_max = Some((replace(&cm.0, from, to), cm.1));
+		//replacements don't change maximums (but do change keys)
+	}
+	new_library
+}
+
+fn find_best_token(library : &Counter<Token>) -> (Token, usize) {
+	let mut can = find_candidate(&library);
+	loop {
+		
+		let maybe_ext = find_best_extention(&library, &can.0);
+		//if we find at least one valid extention
+		if let Some(ext) = maybe_ext {
+			//if the extention occurs often enough
+			if  ext.1 * EXT_RATIO > can.1 {
+			//the candidate is the true extention
+			can = ext
+			} else {
+				break can
+			}
+		} else {
+			//otherwise we found the best candidate
+			break can
+		}
+	}
+}
+
+fn find_candidate(library : &Counter<Token>) -> (Token, usize) {
+	let pair_counts : Counter<Token> = 
+ 		library.par_iter().map(|(t, &weight)|
+			t.windows(2).map(|a| a.to_vec()).collect::<Counter<Token>>() * weight
+		).sum();
+	pair_counts.most_common().unwrap().into()
+}
+
+fn find_best_extention(library : &Counter<Token>, candidate : &Token) -> Option<(Token, usize)> {
+	let extention_counts : Counter<Token> = 
+ 		library.par_iter().map(|(t, &weight)|
+			t.windows(candidate.len()+1)
+			 .filter(
+						|win| win[0..win.len()-1] == *candidate //try extending backwards
+							|| win[1..win.len()] == *candidate //try extending forwards
+				).map(|a| a.to_vec()).collect::<Counter<Token>>() * weight
+		).sum();
+	extention_counts.most_common().into()
+}
+
+fn gen_word_counts<P>(filename : P) -> io::Result<Counter<Token>>
+where P: AsRef<Path>{
+	let lines = read_lines(filename)? ;
+	let word_counts : Counter<Token> =
+		lines.map_while(Result::ok).par_bridge().map(|x| {
+			x.split(' ').map(
+				//turn words into Vec<u16>s
+				|s| s.as_bytes().into_iter().map(|&b| b as u16).collect::<Token>()
+			).collect::<Counter<Token>>()
+		}).sum();
+	return Ok(word_counts);
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
+where P: AsRef<Path>, {
+	let file = File::open(filename)?;
+	Ok(io::BufReader::new(file).lines())
+}
+
+//encode a text by a vocabulary
+fn decode(text : Vec<u16>, vocab : &Vec<Token>) -> Vec<u16> {
+	text.iter().flat_map(|&c| if c < BASE_TOKENS {
+		vec!(c)
+	} else {
+		decode(vocab[(c - BASE_TOKENS) as usize].clone(), vocab)
+	}).collect()
+}
+
+fn find_prefix<'a>(map : &FxHashMap<&[u16], u16>, text: &'a Vec<u16>, start_idx: usize)
+		-> &'a [u16] {
+	let mut i : usize = 0;
+	while i + start_idx < text.len()
+		&& map.contains_key(&text[start_idx .. i + start_idx]) {
+		
+		i += 1;
+	}
+	&text[start_idx .. i + start_idx]
+}
+
+fn encode(text : &Vec<u16>, vocab : &Vec<Token>) -> Vec<u16> {
+	let map : FxHashMap<&[u16], u16> = vocab.iter().enumerate()
+		.map(|(idx, t)| (t.as_slice(),idx as u16)).collect::<FxHashMap<_, u16>>();
+	let mut result : Vec<u16> = Vec::new();
+	let mut i = 0;
+	while i < text.len() {
+		if text[i] < BASE_TOKENS {
+			result.push(text[i]);
+			i += 1;
+		} else {
+			let prefix = find_prefix(&map, &text, i);
+			result.push(map[prefix]);
+			i += prefix.len();
+		}
+	};
+	result
+}
+
+fn to_string(t : &Token) -> String {
+	let x = 
+	t.iter().map(|&u| {
+		if u < 256 { u as u8 } else {'?' as u8}
+	}).collect::<Vec<u8>>();
+	
+	String::from_utf8_lossy(&x).to_string()
+}
+
+fn fertility(library : &Counter<Token>) -> f64 {
+	//take a weighted average of the token lengths
+	let total_token_lengths : usize =
+		library.par_iter().map(|(key, value)| key.len() * value).sum();
+	total_token_lengths as f64 / library.total() as f64
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	
+	#[test]
+	fn test_replace() {
+		let c = vec!(1,2,3,4);
+		let a = replace(&c, &[2,3], 4);
+		assert_eq!(a, [1,4,4]);
+	}
+	#[test]
+	fn test_replace_no_match() {
+		let c = vec!(1,2,3,4);
+		let a = replace(&c, &[4,3], 4);
+		assert_eq!(a, [1,2,3,4]);
+		let a = replace(&c, &[1,2,3,4,5], 4);
+		assert_eq!(a, [1,2,3,4]);
+	}
+	#[test]
+	fn test_replace_double_replace() {
+		let c = vec!(1,2,1,2);
+		let a = replace(&c, &[1,2], 4);
+		assert_eq!(a, [4,4]);
+	}
+	
+	#[test]
+	fn test_find_candidate() {
+		let mut c = Counter::new();
+		c.update(vec!(vec!(1,1,2), vec!(1,1), vec!(1,1,2)));
+		let a = find_candidate(&c);
+		assert_eq!(a.0, vec!(1,1) );
+		assert_eq!(a.1, 3 );
+	}
+
+	#[test]
+	fn test_find_best_extention_right() {
+		let mut c = Counter::new();
+		c.update(vec!(vec!(1,1,2), vec!(1,1), vec!(1,1,2)));
+		let a = find_best_extention(&c, &vec!(1,1));
+		let aa = a.unwrap();
+		assert_eq!(aa.0, vec!(1,1,2) );
+		assert_eq!(aa.1, 2 );
+	}
+
+	#[test]
+	fn test_find_best_extention_left() {
+		let mut c = Counter::new();
+		c.update(vec!(vec!(1,1,2), vec!(1,1), vec!(1,1,2)));
+		let a = find_best_extention(&c, &vec!(1,2));
+		let aa = a.unwrap();
+		assert_eq!(aa.0, vec!(1,1,2) );
+		assert_eq!(aa.1, 2 );
+	}
+
+	#[test]
+	fn test_find_best_extention_empty() {
+		let mut c = Counter::new();
+		c.update(vec!(vec!(1,1,2), vec!(1,1), vec!(1,1,2)));
+		let a = find_best_extention(&c, &vec!(1,1,2));
+		std::assert_matches!(a, None);
+	}
+
+	#[test]
+	fn test_find_best_token() {
+		let mut c = Counter::new();
+		c.update(vec!(vec!(1,1,2), vec!(1,1), vec!(1,1,2)));
+		let a = find_best_token(&c);
+		assert_eq!(a.0, vec!(1,1,2) );
+		assert_eq!(a.1, 2 );
+	}
+
+	#[test]
+	fn test_replace_in_library() {
+		let mut c = Counter::new();
+		c.update(vec!(vec!(1,1,2), vec!(1,1), vec!(1,1,2)));
+		let a = replace_in_library(&c, &[1,2], 3);
+		let mut b = Counter::new();
+		b.update(vec!(vec!(1,3), vec!(1,1), vec!(1,3)));
+		assert_eq!(a, b);
+	}
+	
+	#[test]
+	fn test_encode_decode() {
+		let text = vec!(1,2,1);
+		let vocab = vec!(vec!(1,2));
+		let encoded_text = encode(&text, &vocab);
+		let final_text = decode(encoded_text, &vocab);
+		assert_eq!(text, final_text);
+	}
+}
+// vim: ts=2 sw=2
